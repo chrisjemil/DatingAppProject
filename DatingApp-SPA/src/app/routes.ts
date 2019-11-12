02184ae084eee6241218c55fd42b20e1fe62089a
Christin Jemildan Anton Yogarajah <jemildan@gmail.com>
2019-11-12 13:15:06 +0000

    added Angular route and auth guard to protect unauthorised access

---
+++ DatingApp-SPA/src/app/routes.ts
@@ -0,0 +1,22 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
+import { AuthGuard } from './_guards/auth.guard';
+
+// Setting up the Angular routes
+export const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard], // unautharised not allowed to children routes
+    children: [
+      { path: 'members', component: MemberListComponent },
+      { path: 'messages', component: MessagesComponent },
+      { path: 'lists', component: ListsComponent }
+    ]
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
