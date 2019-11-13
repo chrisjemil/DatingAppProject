85d2925c814cbaca1600a08a240089d7f0fa1413
Christin Jemildan Anton Yogarajah <jemildan@gmail.com>
2019-11-13 17:21:54 +0000

    created memeber card to display member list and added animated buttons

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -12,10 +12,12 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AlertifyService } from './_services/alertify.service';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { appRoutes } from './routes';
+import { UserService } from './_services/user.service';
+import { MemberCardComponent } from './members/member-card/member-card.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,8 @@ import { appRoutes } from './routes';
     RegisterComponent,
     MemberListComponent,
     MessagesComponent,
-    ListsComponent
+    ListsComponent,
+    MemberCardComponent
   ],
   imports: [
     BrowserModule,
@@ -34,7 +37,7 @@ import { appRoutes } from './routes';
     BsDropdownModule.forRoot(),
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [AuthService, ErrorInterceptorProvider, AlertifyService],
+  providers: [AuthService, ErrorInterceptorProvider, AlertifyService, UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
