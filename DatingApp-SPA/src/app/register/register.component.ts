d151deb1a6f4b24e8b841d503c7706cdc5ee464d
Christin Jemildan Anton Yogarajah <jemildan@gmail.com>
2019-11-16 15:44:55 +0000

    added photo upload to new user and completed registration form

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,13 +1,10 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
-import {
-  FormGroup,
-  FormControl,
-  Validators,
-  FormBuilder
-} from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { BsDatepickerConfig } from 'ngx-bootstrap';
+import { User } from '../_models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -16,12 +13,13 @@ import { BsDatepickerConfig } from 'ngx-bootstrap';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
+  user: User;
   registerForm: FormGroup;
   bsConfig: Partial<BsDatepickerConfig>;
 
   constructor(
     private authService: AuthService,
+    private router: Router,
     private alertify: AlertifyService,
     private fb: FormBuilder
   ) {}
@@ -29,7 +27,7 @@ export class RegisterComponent implements OnInit {
   ngOnInit() {
     this.bsConfig = {
       containerClass: 'theme-red'
-    }
+    };
     this.createRegisterForm();
   }
 
@@ -42,14 +40,7 @@ export class RegisterComponent implements OnInit {
         dateOfBirth: [null, Validators.required],
         city: ['', Validators.required],
         country: ['', Validators.required],
-        password: [
-          '',
-          [
-            Validators.required,
-            Validators.minLength(4),
-            Validators.maxLength(8)
-          ]
-        ],
+        password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
         confirmPassword: ['', Validators.required]
       },
       { validator: this.passwordMatchValidator }
@@ -57,18 +48,26 @@ export class RegisterComponent implements OnInit {
   }
 
   passwordMatchValidator(g: FormGroup) {
-    return g.get('password').value === g.get('confirmPassword').value
-      ? null
-      : { mismatch: true };
+    return g.get('password').value === g.get('confirmPassword').value ? null : { mismatch: true };
   }
 
   register() {
-    // this.authService.register(this.model).subscribe(() => {
-    //   this.alertify.success('Registration successful');
-    // }, error => {
-    //   this.alertify.error(error);
-    // });
-    console.log(this.registerForm.value);
+    if (this.registerForm.valid) {
+      this.user = Object.assign({}, this.registerForm.value);
+      this.authService.register(this.user).subscribe(
+        () => {
+          this.alertify.success('Registration successful.');
+        },
+        error => {
+          this.alertify.error(error);
+        },
+        () => {
+          this.authService.login(this.user).subscribe(() => {
+            this.router.navigate(['/members']);
+          });
+        }
+      );
+    }
   }
 
   cancel() {
