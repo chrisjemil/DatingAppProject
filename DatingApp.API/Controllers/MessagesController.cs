f719cf86d3a6937b64ac3e0371e1bd9390f30717
Christin Jemildan Anton Yogarajah <jemildan@gmail.com>
2019-11-21 16:00:00 +0000

    added message thread method to API

--- DatingApp.API/Controllers/MessagesController.cs
+++ DatingApp.API/Controllers/MessagesController.cs
@@ -61,6 +61,19 @@ namespace DatingApp.API.Controllers
 
         }
 
+        [HttpGet("thread/{recipientId}")]
+        public async Task<IActionResult> GetMessageThread(int userId, int recipientId)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+            
+            var messageFromRepo = await _repo.GetMessageThread(userId, recipientId);
+
+            var messageThread = _mapper.Map<IEnumerable<MessageToReturnDto>>(messageFromRepo);
+            
+            return Ok(messageThread);
+        }
+
         // create message
         [HttpPost]
         public async Task<IActionResult> CreateMessage(int userId, MessageForCreationDto messageForCreationDto)
