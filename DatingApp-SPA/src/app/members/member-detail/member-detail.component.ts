4237881874da067babfbe352477db3f45227d531
Christin Jemildan Anton Yogarajah <jemildan@gmail.com>
2019-11-21 19:35:57 +0000

    added query params to Angular

--- DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
+++ DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/_models/user';
-import { UserService } from 'src/app/_services/user.service';
-import { AlertifyService } from 'src/app/_services/alertify.service';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { User } from "src/app/_models/user";
+import { UserService } from "src/app/_services/user.service";
+import { AlertifyService } from "src/app/_services/alertify.service";
+import { ActivatedRoute } from "@angular/router";
 import {
   NgxGalleryOptions,
   NgxGalleryImage,
   NgxGalleryAnimation
-} from 'ngx-gallery';
+} from "ngx-gallery";
+import { TabsetComponent } from "ngx-bootstrap";
 
 @Component({
-  selector: 'app-member-detail',
-  templateUrl: './member-detail.component.html',
-  styleUrls: ['./member-detail.component.css']
+  selector: "app-member-detail",
+  templateUrl: "./member-detail.component.html",
+  styleUrls: ["./member-detail.component.css"]
 })
 export class MemberDetailComponent implements OnInit {
+  @ViewChild("memberTabs", { static: true }) memberTabs: TabsetComponent;
   user: User;
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
@@ -26,13 +28,18 @@ export class MemberDetailComponent implements OnInit {
 
   ngOnInit() {
     this.route.data.subscribe(data => {
-      this.user = data['user'];
+      this.user = data["user"];
+    });
+
+    this.route.queryParams.subscribe(params => {
+      const selectedTab = params["tab"];
+      this.memberTabs.tabs[selectedTab > 0 ? selectedTab : 0].active = true;
     });
 
     this.galleryOptions = [
       {
-        width: '500px',
-        height: '500px',
+        width: "500px",
+        height: "500px",
         imagePercent: 100,
         thumbnailsColumns: 4,
         imageAnimation: NgxGalleryAnimation.Slide,
@@ -55,6 +62,9 @@ export class MemberDetailComponent implements OnInit {
     return imageUrls;
   }
 
+  selectTab(tabId: number) {
+    this.memberTabs.tabs[tabId].active = true;
+  }
   // get users' details
   // loadUser() {
   //   this.userService.getUser(+this.route.snapshot.params['id']).subscribe(
