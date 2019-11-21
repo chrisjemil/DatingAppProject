cda682820aa732e5bac8d0ce1943effedde0c42f
Christin Jemildan Anton Yogarajah <jemildan@gmail.com>
2019-11-21 21:12:51 +0000

    added the mark as read functionality

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -142,4 +142,13 @@ export class UserService {
       {}
     );
   }
+
+  markAsRead(userId: number, messageId: number) {
+    this.http
+      .post(
+        this.baseUrl + "users/" + userId + "/messages/" + messageId + "/read",
+        {}
+      )
+      .subscribe();
+  }
 }
